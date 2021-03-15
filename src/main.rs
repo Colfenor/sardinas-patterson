09bd8b3dc5020a7b1bc85137fd2058b7939e6d3f
Bernhard <34011017+Colfenor@users.noreply.github.com>
2021-03-15 18:51:58 +0100

    add input file and first parsing attempt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
+use std::env;
+use std::path::Path;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
 
 
 fn main() {
+    // pass filename to cmd line args
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 
-    println!("Read in file !");
+    if args.len() == 1 {
+        println!("Please specify the input file as second parameter");
+        return;
+    }
+
+    let path = Path::new(&args[1]);
+    if let Ok(lines) = read_lines(path) {
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("{}", ip);
+                
+            }
+        }
+    }
+
+
+    
 }
