6aa3228c58cf1f78286e9097efe7ed950e4cebc3
Bernhard <34011017+Colfenor@users.noreply.github.com>
2021-03-17 00:59:18 +0100

    Refactor parsing file into separate function

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,24 @@ use std::fs::File;
 use std::io::prelude::Read;
 
 /* sardinas patterson algorithm for testing unique decipherability */
-
 // reference: IEEE TRANSACTIONS ON INFORMATION THEORY, VOL. IT-28, NO. 4, JULY 1982
+
+fn read_in_file(path: &Path) -> Vec<String> {
+    
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut parsed_content = String::new();
+    match file.read_to_string(&mut parsed_content) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:{} \n", display, parsed_content),
+    }
+
+    parsed_content.split(",").map(str::to_string).collect()
+}
 
 fn main() {
     // pass filename to cmd line args
@@ -18,23 +34,10 @@ fn main() {
     }
 
     let path = Path::new(&args[1]);
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
-
-    let mut parsed_content = String::new();
-    match file.read_to_string(&mut parsed_content) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => print!("{} contains:{} \n", display, parsed_content),
-    }
-
-    let input_codes: Vec<&str> = parsed_content.split(",").collect();
     
-    for x in input_codes {
+    let test = read_in_file(path);
+
+    for x in test {
         println!("{}\n", x)
     }
-
 }
