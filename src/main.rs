0dcb7ffbfc8f6f66d19d24e1f938e962b735ecce
Bernhard <34011017+Colfenor@users.noreply.github.com>
2021-03-27 20:21:34 +0100

    Check for dups, refactor into function

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,67 @@
-use std::env;
+use std::{env, vec};
 use std::path::Path;
 use std::fs::File;
 use std::io::prelude::Read;
+use std::process::exit;
 
 /* sardinas patterson algorithm for testing unique decipherability */
 // reference: IEEE TRANSACTIONS ON INFORMATION THEORY, VOL. IT-28, NO. 4, JULY 1982
 
-// 01,10,1001
+//01,10,1001
 
 fn read_in_file(path: &Path) -> Vec<String> {
     
     let display = path.display();
     let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Err(why) => panic!("could not open {}: {}", display, why),
         Ok(file) => file,
     };
 
     let mut parsed_content = String::new();
     match file.read_to_string(&mut parsed_content) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => print!("{} contains:{} \n", display, parsed_content),
+        Err(why) => panic!("could not read {}: {}", display, why),
+        Ok(_) => print!("{} contains: {} \n", display, parsed_content),
     }
 
     parsed_content.split(",").map(str::to_string).collect()
+}
+
+fn duplicates_inside(vector: &Vec<String>) -> bool {
+    let mut vector_copy = vec![];
+
+    for x in vector {
+        if vector_copy.contains(x) {
+            return true;
+        }
+        vector_copy.push(String::from(x));
+    }
+    false
+}
+
+fn sardinas_patterson_algorithm(codeword_list: &Vec<String>) -> bool {
+
+    let mut tails = vec![];
+
+    //E1.1 check for duplicates in our list of codewords
+    if duplicates_inside(&codeword_list) {
+        println!("duplicate word detected \n");
+        return false;
+    }
+
+    // E1.2
+    for i in codeword_list {
+        for j in codeword_list {
+            if i != j && i.chars().count() > j.chars().count() && i.find(j) == Some(0) {
+                // E1.1
+                //todo extract suffix and save in vector
+                let suffix = &i[j.chars().count()..];
+                tails.push(String::from(suffix));
+                println!("{}\n", suffix);
+            }
+        }
+    }
+
+    true
 }
 
 fn main() {
@@ -30,25 +69,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    if args.len() == 1 {
+    if args.len() != 2 {
         println!("Please specify the input file as second parameter");
-        return;
+        exit(1);
     }
 
     let path = Path::new(&args[1]);
-    
-    let test = read_in_file(path);
-    //let output: Vec<&str>;
 
-    for x in &test {
-        for y in &test {
-            if x.chars().count() > y.chars().count() && x.find(y) == Some(0) {
-                //
-            }
-        }
-    }
-
-    /*for t in output {
-        println!("{}\n", t);
-    }*/
+    sardinas_patterson_algorithm(&read_in_file(path));
 }
